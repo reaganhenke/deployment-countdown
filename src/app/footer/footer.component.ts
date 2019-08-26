8afc8acd4370117e94e084776da9509a6f797cfc
Reagan Henke <reaganhenke@Reagans-MBP.lan>
2019-08-25 20:26:23 -0400

    footer design

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ShareModalComponent } from '../share-modal/share-modal.component';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit() {
   }
 
+  openShare() {
+    this.dialog.open(ShareModalComponent);
+  }
 }
